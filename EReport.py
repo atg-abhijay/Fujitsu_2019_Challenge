e3b36c2b61aece0f7c7309559f826dcc0b489523
Abhijay Gupta <atg.abhijay@gmail.com>
2019-02-21 00:41:20 -0500

    corrected printing format; simplified code; still have to check about modifying original data file

--- EReport.py
+++ EReport.py
@@ -5,13 +5,17 @@ def main():
     with open('employees.dat') as infile:
         df = pd.read_csv(infile, comment='#', header=None, names=['emp_number', 'emp_first_name', 'emp_last_name'], sep=" |,", engine='python')
 
-    print(df.head())
-    print("\nProcessing by employee number...")
     sorted_by_num = df.sort_values(by=['emp_number'])
-    print(sorted_by_num)
-    print("\nProcessing by last (family) Name...")
-    sorted_by_last_name = df.sort_values(by=['emp_last_name']).to_csv(index=False, columns=None)
-    print(sorted_by_last_name)
+    print_in_format("Processing by employee number...", sorted_by_num)
+    print()
+    sorted_by_last_name = df.sort_values(by=['emp_last_name'])
+    print_in_format("Processing by last (family) Name...", sorted_by_last_name)
+
+
+def print_in_format(message, dataframe):
+    print(message)
+    for record in dataframe.itertuples(index=False, name=None):
+        print(str(record[0]) + ',' + record[1] + ' ' + record[2])
 
 
 if __name__ == '__main__':
